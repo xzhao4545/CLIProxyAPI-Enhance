eb722f0debc87bf24c5f2f283e59f30629de6388
xzhao <1587970885@qq.com>
2026-05-29 14:46:44 +0800

    doc: 默认面板地址改为 xzhao4545 的 fork 仓库

--- internal/config/config.go
+++ internal/config/config.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	DefaultPanelGitHubRepository = "https://github.com/router-for-me/Cli-Proxy-API-Management-Center"
+	DefaultPanelGitHubRepository = "https://github.com/xzhao4545/Cli-Proxy-API-Management-Center-Ehance"
 	DefaultPprofAddr             = "127.0.0.1:8316"
 	DefaultAuthDir               = "~/.cli-proxy-api"
 )
